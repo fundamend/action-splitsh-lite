6ab8a658b498a09fa265a7468bfad371cad3e434
- <gitkraken@mail.decouplr.com>
2021-05-08 19:39:00 +0200

    fix: change permissions of .ssh directory to 0700

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,6 +2,7 @@
 
 ssh-keyscan github.com > /src/.ssh/known_hosts
 echo "${INPUT_TARGET_DEPLOY_KEY}" > /src/.ssh/github
+chmod --recursive 0700 /src/.ssh
 chmod 0600 /src/.ssh/github
 
 splitsh-lite --prefix="${INPUT_SOURCE_DIRECTORY}" --target=refs/heads/${INPUT_TARGET_REPOSITORY} --scratch
